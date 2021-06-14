259c4d62392150827800df8c080bddda60001a18
kashmira <kashmirabhake@gmail.com>
2021-06-14 21:19:42 +0530

    initial commit

---
+++ src/test/java/com/restapikarkinos/WebappApi/WebappApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.restapikarkinos.WebappApi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebappApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
