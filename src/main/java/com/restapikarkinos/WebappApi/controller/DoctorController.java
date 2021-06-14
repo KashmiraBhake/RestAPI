7d11bba5183735f367e187e140acfae2f2999f00
tanyagohit25 <gogohittanya@gmail.com>
2021-06-14 19:43:15 +0000

    wip controller

--- src/main/java/com/restapikarkinos/WebappApi/controller/DoctorController.java
+++ src/main/java/com/restapikarkinos/WebappApi/controller/DoctorController.java
@@ -1,5 +1,50 @@
 package com.restapikarkinos.WebappApi.controller;
 
+import java.util.List;
+
+import com.restapikarkinos.WebappApi.model.Doctor;
+import com.restapikarkinos.WebappApi.repository.DoctorRepository;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/doctor")
 public class DoctorController {
     
+    @Autowired
+    DoctorRepository doctorRepository;
+
+    //get information from view to controller using a form
+    @GetMapping("/new_doctor")
+    public String new_doctor() {
+
+       return "new_doctor";
+    }
+
+    @GetMapping("/view_all_doctor")
+    public List<Doctor> getDoctors() {
+
+      
+      
+    }
+
+    @PostMapping("/create_new_doctor")
+    public ResponseEntity<Doctor> createDoctor(@RequestBody Doctor doctor) {
+    try {
+        System.out.println("hello");
+        Doctor _doctor = doctorRepository.save(new Doctor(doctor.getFirstName(), doctor.getLastName(), doctor.getSpecialization(), doctor.getPhoneNumber(), doctor.getAddress(), doctor.getCity(), doctor.getPincode()));
+        return new ResponseEntity<>(_doctor, HttpStatus.CREATED);
+      } catch (Exception e) {
+        return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
+      }
+    }
+
+
 }
--- src/main/java/com/restapikarkinos/WebappApi/model/Doctor.java
+++ src/main/java/com/restapikarkinos/WebappApi/model/Doctor.java
@@ -11,29 +11,29 @@ public class Doctor {
   @Id
   private String id;
 
-  @NotBlank
-  @Pattern(regexp = "^[a-zA-Z0-9'.]{3,50}$" , message = "only alphabets, numbers, apostrophe, dot characters are allowed, Firstname must be 3-50 characters long")
+  // @NotBlank
+  // @Pattern(regexp = "^[a-zA-Z0-9'.]{3,50}$" , message = "only alphabets, numbers, apostrophe, dot characters are allowed, Firstname must be 3-50 characters long")
   private String firstName;
 
-  @NotBlank
-  @Pattern(regexp = "^[a-zA-Z0-9'.]{3,50}$",message = "only alphabets, numbers, apostrophe, dot characters are allowed, Firstname must be 3-50 characters long")
+  // @NotBlank
+  // @Pattern(regexp = "^[a-zA-Z0-9'.]{3,50}$",message = "only alphabets, numbers, apostrophe, dot characters are allowed, Firstname must be 3-50 characters long")
   private String lastName;
 
-  @NotBlank
-  @Pattern(regexp = "^[A-Za-z]{3,100}$", message = "only alphabets are allowed")
+  // @NotBlank
+  // @Pattern(regexp = "^[A-Za-z]{3,100}$", message = "only alphabets are allowed")
   private String specialization;
 
-  @Pattern(regexp="^[1-9][0-9]{9}$", message="Mobile number is invalid")
+  // @Pattern(regexp="^[1-9][0-9]{9}$", message="Mobile number is invalid")
   private String phoneNumber;
 
-  @NotBlank
-  @Pattern(regexp = "^[a-zA-Z0-9,'.]{10,2000}$",message = "Address must be min 10 and max 2000 chars long. only alphabets, numbers, comma, dot, apostrophe characters are allowed")
+  // @NotBlank
+  // @Pattern(regexp = "^[a-zA-Z0-9,'.]{10,2000}$",message = "Address must be min 10 and max 2000 chars long. only alphabets, numbers, comma, dot, apostrophe characters are allowed")
   private String address;
 
-  @Pattern(regexp = "^[A-Za-z]{3,100}$", message = "Please enter between {min} and {max}")
+  // @Pattern(regexp = "^[A-Za-z]{3,100}$", message = "Please enter between {min} and {max}")
   private String city;
 
-  @Pattern(regexp="^[1-9][0-9]{5}$", message="Pincode is invalid")
+  // @Pattern(regexp="^[1-9][0-9]{5}$", message="Pincode is invalid")
   private String pincode;
 
   public Doctor() {
