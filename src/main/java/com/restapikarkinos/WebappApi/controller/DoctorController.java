cfe460a28455a15856b310fcf2c58977f0009a5a
kbgithub7 <kgithub7@gmail.com>
2021-06-16 03:26:06 +0000

    view all patient

--- src/main/java/com/restapikarkinos/WebappApi/controller/DoctorController.java
+++ src/main/java/com/restapikarkinos/WebappApi/controller/DoctorController.java
@@ -1,50 +1,96 @@
 package com.restapikarkinos.WebappApi.controller;
 
+import java.util.Arrays;
 import java.util.List;
 
+import javax.validation.Valid;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonMappingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
 import com.restapikarkinos.WebappApi.model.Doctor;
-import com.restapikarkinos.WebappApi.repository.DoctorRepository;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.client.RestClientException;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.web.servlet.ModelAndView;
 
-@RestController
+@Controller
 @RequestMapping("/doctor")
 public class DoctorController {
+  private static final String GET_ALL_DOCTORS_API = "https://8080-emerald-whale-xtd7vopp.ws-us09.gitpod.io/api/doctors";
+  private static final String CREATE_DOCTOR_API = "https://8080-emerald-whale-xtd7vopp.ws-us09.gitpod.io/api/doctors";
+
+    static RestTemplate restTemplate = new RestTemplate();
+
+    //***************************NEW DOCTOR FORM************************************************* */
+
+    @RequestMapping("/new_doctor")
+    public ModelAndView new_doctor() {
+        ModelAndView modelAndView = new ModelAndView();
+        modelAndView.setViewName("new_doctor");
+
+       return modelAndView;
+    }
+
+    //***************************NEW DOCTOR SUBMIT************************************************ */
+    // @RequestMapping(path="/create_new_doctor",method=RequestMethod.POST)
+    // public ModelAndView create_new_doctor(@Valid @ModelAttribute Doctor doctor, BindingResult bindingResult,
+    //     @RequestParam String firstName,
+    //     @RequestParam String lastName,
+    //     @RequestParam String specialization,
+    //     @RequestParam String phoneNumber,
+    //     @RequestParam String address,
+    //     @RequestParam String city,
+    //     @RequestParam String pincode) {
+    //       ModelAndView modelAndView = new ModelAndView();
+
+    //         if (bindingResult.hasErrors()) {       
+        
+    //             System.out.println(bindingResult);
+                
+    //             modelAndView.setViewName("new_doctor");
+    //             return modelAndView;
+    //         }
+    //     modelAndView.setViewName("submit_doctor");
+    //     modelAndView.addObject("firstName", firstName);
+    //     modelAndView.addObject("lastName", lastName);
+    //     modelAndView.addObject("specialization", specialization);
+    //     modelAndView.addObject("phoneNumber", phoneNumber);
+    //     modelAndView.addObject("address", address);
+    //     modelAndView.addObject("city", city);
+    //     modelAndView.addObject("pincode", pincode);
+
+    //     return modelAndView;
+    // }
+
+    //***************************VIEW ALL DOCTORS************************************************* */
     
-    @Autowired
-    DoctorRepository doctorRepository;
+    @RequestMapping(value="/view_all_doc",method=RequestMethod.GET)
+     private ModelAndView callGetAllDoctorsAPI() throws JsonMappingException, JsonProcessingException, RestClientException{
 
-    //get information from view to controller using a form
-    @GetMapping("/new_doctor")
-    public String new_doctor() {
+        HttpHeaders headers = new HttpHeaders();
+        headers.setAccept(Arrays.asList(MediaType.APPLICATION_JSON));
 
-       return "new_doctor";
-    }
+        //Doctor[] result = restTemplate.getForObject(GET_ALL_DOCTORS_API,Doctor[].class);
+        ObjectMapper mapper = new ObjectMapper();
+        List<Doctor> result = Arrays.asList(mapper.readValue(restTemplate.getForObject(GET_ALL_DOCTORS_API, String.class),Doctor[].class));
+        System.out.println(result.get(0).getFirstName());
 
-    @GetMapping("/view_all_doctor")
-    public List<Doctor> getDoctors() {
-
-      
-      
-    }
-
-    @PostMapping("/create_new_doctor")
-    public ResponseEntity<Doctor> createDoctor(@RequestBody Doctor doctor) {
-    try {
-        System.out.println("hello");
-        Doctor _doctor = doctorRepository.save(new Doctor(doctor.getFirstName(), doctor.getLastName(), doctor.getSpecialization(), doctor.getPhoneNumber(), doctor.getAddress(), doctor.getCity(), doctor.getPincode()));
-        return new ResponseEntity<>(_doctor, HttpStatus.CREATED);
-      } catch (Exception e) {
-        return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
-      }
-    }
+        ModelAndView modelAndView = new ModelAndView();
+        modelAndView.setViewName("view_all_doctor");
+        modelAndView.addObject("doctors", result);
+  
+        return modelAndView;
+     }
 
 
 }
